b7d95ee044400d30d61ac3e97d1929589274d8ac
Nicolas Mermoud <105772504+mehu3dhokia@users.noreply.github.com>
2025-04-27 00:28:02 +0800

    Update customize.sh
    Roll back to the file before the real vbmetasize

--- customize.sh
+++ customize.sh
@@ -121,17 +121,6 @@ for i in $files ; do
     rm $MODPATH/$i
 done
 
-# Random ro.boot.vbmeta.size value
-# vbmeta_size=$(( 5504 + (RANDOM % 14 + 1)*1024 ))  # 5504~16384, 1KB对齐
-vbmeta_size="$(( RANDOM % (7000 - 1000 + 1) + 3000 ))"
-
-# Precisely overwrite vbmeta_size in config
-if grep -q "^vbmeta_size=" /data/adb/susfs4ksu/config.sh; then
-    sed -i "s/^vbmeta_size=.*/vbmeta_size=$vbmeta_size/" /data/adb/susfs4ksu/config.sh
-else
-    echo "vbmeta_size=$vbmeta_size" >> /data/adb/susfs4ksu/config.sh
-fi
-
 rm -rf ${MODPATH}/tools
 rm ${MODPATH}/customize.sh ${MODPATH}/README.md
 
