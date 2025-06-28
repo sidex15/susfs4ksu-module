27f707e44d04e194f68f1a6b0b063be30a0f6aa4
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-06-28 13:14:14 +0800

    scripts: service: comment out add_sus_kstat and update_sus_kstat in hide vendor, compat matrix, and spoof service.
    The culprit of the EACCES detection is the add_sus_kstat add update_sus_kstat.
    For now we will comment them out to avoid the EACCES detection.
    
    note: This is a temporary fix or a workaround until we can find a better solution.

--- service.sh
+++ service.sh
@@ -178,10 +178,10 @@ fi
 		grep -q lineage $sepolicy_cil && {
 			cil_name=`basename "$sepolicy_cil"`
 			grep -v "lineage" $sepolicy_cil > $mntfolder/$cil_name
-			${SUSFS_BIN} add_sus_kstat $sepolicy_cil && echo "[update_sus_kstat]: susfs4ksu/service $sepolicy_cil" >> $logfile1
+			#${SUSFS_BIN} add_sus_kstat $sepolicy_cil && echo "[update_sus_kstat]: susfs4ksu/service $sepolicy_cil" >> $logfile1
 			susfs_clone_perm $mntfolder/$cil_name $sepolicy_cil
 			mount --bind $mntfolder/$cil_name $sepolicy_cil
-			${SUSFS_BIN} update_sus_kstat $sepolicy_cil && echo "[update_sus_kstat]: susfs4ksu/service $sepolicy_cil" >> $logfile1
+			#${SUSFS_BIN} update_sus_kstat $sepolicy_cil && echo "[update_sus_kstat]: susfs4ksu/service $sepolicy_cil" >> $logfile1
 			${SUSFS_BIN} add_sus_mount $sepolicy_cil && echo "[sus_mount]: susfs4ksu/service $sepolicy_cil" >> $logfile1
 		}
 	done
@@ -193,10 +193,10 @@ fi
 	compatibility_matrix=/system/etc/vintf/compatibility_matrix.device.xml
 	grep -q lineage $compatibility_matrix && {
 		grep -v "lineage" $compatibility_matrix > $mntfolder/compatibility_matrix.device.xml
-		${SUSFS_BIN} add_sus_kstat $compatibility_matrix && echo "[update_sus_kstat]: susfs4ksu/service $compatibility_matrix" >> $logfile1
+		#${SUSFS_BIN} add_sus_kstat $compatibility_matrix && echo "[update_sus_kstat]: susfs4ksu/service $compatibility_matrix" >> $logfile1
 		susfs_clone_perm $mntfolder/compatibility_matrix.device.xml $compatibility_matrix
 		mount --bind $mntfolder/compatibility_matrix.device.xml $compatibility_matrix
-		${SUSFS_BIN} update_sus_kstat $compatibility_matrix && echo "[update_sus_kstat]: susfs4ksu/service $compatibility_matrix" >> $logfile1
+		#${SUSFS_BIN} update_sus_kstat $compatibility_matrix && echo "[update_sus_kstat]: susfs4ksu/service $compatibility_matrix" >> $logfile1
 		${SUSFS_BIN} add_sus_mount $compatibility_matrix && echo "[sus_mount]: susfs4ksu/service $compatibility_matrix" >> $logfile1
 	}
 }
@@ -210,9 +210,9 @@ fi
 	echo "FAKELIST=\"$(/system/bin/service list | sed 's/lineage//g; s/Lineage//g' | base64 -w 0)"\" >> "$mntfolder/system_bin/service"
 	echo "echo \$FAKELIST | base64 -d" >> "$mntfolder/system_bin/service"
 	susfs_clone_perm "$mntfolder/system_bin/service" /system/bin/service
-	${SUSFS_BIN} add_sus_kstat /system/bin/service
+	#${SUSFS_BIN} add_sus_kstat /system/bin/service
 	mount --bind "$mntfolder/system_bin/service" /system/bin/service
-	${SUSFS_BIN} update_sus_kstat /system/bin/service
+	#${SUSFS_BIN} update_sus_kstat /system/bin/service
 	${SUSFS_BIN} add_sus_mount /system/bin/service
 }
 
