566f75c04d3108bad65c49812565847e0a0cda6b
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-07-06 20:16:02 +0800

    scripts: add susfs binary check script for WebUI

---
+++ susfs-bin-check.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
+SUSFSD=/data/adb/ksu/bin/susfsd
+TMPDIR=/data/adb/ksu/susfs4ksu
+
+download() { busybox wget -T 1 --no-check-certificate -qO - "$1"; }
+if command -v curl > /dev/null 2>&1; then
+	download() { curl --connect-timeout 1 -Ls "$1"; }
+fi
+
+check() { 
+    if command -v curl > /dev/null 2>&1; then
+        curl -s --max-time 1 --head "$1" > /dev/null 2>&1
+    else
+        busybox wget --no-check-certificate --spider -q "$1" > /dev/null 2>&1
+    fi
+}
+
+# Check connectivity first
+base_url="https://raw.githubusercontent.com/sidex15/susfs4ksu-binaries/main"
+if ! check "$base_url"; then
+  echo "no-conn"
+  exit 1
+fi
+
+# Check the hash of susfs binaries
+hash=$(sha256sum ${SUSFS_BIN} | awk '{print $1}')
+cloudhash=$(download https://raw.githubusercontent.com/sidex15/susfs4ksu-binaries/main/$1/$2/ksu_susfs_arm64 | sha256sum | awk '{print $1}')
+[ $hash = $cloudhash > /dev/null 2>&1 ] && echo "match" || echo "mismatch"
+#EOL
