1052b62f0785bb34ec7873cd2650e6aac841445e
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-08-22 13:18:17 +0800

    scripts: boot-completed: use susfsd for checking susfs version

--- boot-completed.sh
+++ boot-completed.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 MODDIR=/data/adb/modules/susfs4ksu
 SUSFS_BIN=/data/adb/ksu/bin/ksu_susfs
+SUSFSD=/data/adb/ksu/bin/susfsd
 KSU_BIN=/data/adb/ksu/bin/ksud
 . ${MODDIR}/utils.sh
 PERSISTENT_DIR=/data/adb/susfs4ksu
 tmpfolder=/data/adb/ksu/susfs4ksu
 logfile="$tmpfolder/logs/susfs.log"
 logfile1="$tmpfolder/logs/susfs1.log"
-version=$(${SUSFS_BIN} show version)
+version=$(${SUSFSD} version)
 SUSFS_DECIMAL=$(echo "$version" | sed 's/^v//; s/\.//g')
 kernel_ver=$(head -n 1 "$PERSISTENT_DIR/kernelversion.txt")
 
