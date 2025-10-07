aea802dc6da583c0fad7ef9a17ae14d5c4faba02
pershoot <190600+pershoot@users.noreply.github.com>
2025-10-07 00:23:57 -0400

    remove Magic Mount support
    -https://gitlab.com/simonpunk/susfs4ksu/-/commit/4ee6ef28d01f158c8d66ab7dc5f43727a434c27c

--- index.js
+++ index.js
@@ -1145,8 +1145,7 @@ async function loadKernelFeatureStatus(susfs_features) {
     { id: 'status_hide_symbols', config: 'CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS' },
     { id: 'status_spoof_cmdline', config: 'CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG' },
     { id: 'status_open_redirect', config: 'CONFIG_KSU_SUSFS_OPEN_REDIRECT' },
-    { id: 'status_magic_mount', config: 'CONFIG_KSU_SUSFS_HAS_MAGIC_MOUNT' },
-	{ id: 'status_overlayfs_auto_kstat', config: 'CONFIG_KSU_SUSFS_SUS_OVERLAYFS' }
+    { id: 'status_overlayfs_auto_kstat', config: 'CONFIG_KSU_SUSFS_SUS_OVERLAYFS' }
   ];
 
   for (const feature of features) {
