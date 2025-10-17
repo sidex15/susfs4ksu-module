3a1dba73977b569bb3c781287c135b2b4449dd97
sidex15 <24408329+sidex15@users.noreply.github.com>
2025-10-17 16:31:39 +0800

    Implement Send Logs to SUSFS Developers
    To make debugging easier to the SUSFS developers, Implement a Send Logs button at the bottom of the screen.
    The logs and other forms of debugging results will be saved as `susfs_logs.tar.gz` to /sdcard/
    
    susfs_logs.tar.gz contains the following:
    1. All of the SUSFS stats and boot/dmesg logs (until boot-completed)
    2. Latest Dmesg Log of SUSFS
    3. Mountinfo and Maps of Zygote64
    4. Mountinfo of PID 1
    5. Dmesg Log of KSU

--- index.js
+++ index.js
@@ -1216,7 +1216,26 @@ function susfs_reset(){
 	});
 }
 
+function susfs_send_logs(){
+	const susfs_send_logs_btn = document.getElementById('susfs_send_logs');
+	susfs_send_logs_btn.addEventListener('click', async function(event) {
+		try {
+		await run(`cat /proc/$(pidof zygote64)/mountinfo > /data/adb/ksu/susfs4ksu/zygote64_mountinfo.txt`);
+		await run(`cat /proc/$(pidof zygote64)/maps > /data/adb/ksu/susfs4ksu/zygote64_maps.txt`);
+		await run(`cat /proc/1/mountinfo > /data/adb/ksu/susfs4ksu/pid1_mountinfo.txt`);
+		await run(`cp /data/adb/ksu/log/dmesg.log /data/adb/ksu/susfs4ksu/dmesg.log`);
+		await run(`dmesg | grep susfs > /data/adb/ksu/susfs4ksu/latest_dmesg_susfs.log`);
+		await run(`tar -C /data/adb/ksu/susfs4ksu/ -czvf /sdcard/susfs_logs.tar.gz .`);
+		toast("Logs saved to /sdcard/susfs_logs.tar.gz");
+		await run(`am start -a android.intent.action.SEND -t '*/*' -c android.intent.category.DEFAULT --eu android.intent.extra.STREAM 'file:///sdcard/susfs_logs.tar.gz'`);
+		} catch (error) {
+			toast("Failed to prepare logs: " + error.message);
+		}
+	});
+}
+
 // Initialize the page
+susfs_send_logs();
 susfs_reset();
 set_uname(settings);
 susfs_log_toggle(settings);
